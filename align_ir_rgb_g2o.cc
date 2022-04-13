d29606189eafe745ac42632076e567ae784f9900
lee-shun <lee970802@163.com>
2022-04-13 16:33:45 -0400

    update

---
+++ align_ir_rgb_g2o.cc
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+ *
+ *   @Filename: align_ir_rgb_g2o.cc
+ *
+ *   @Author: Shun Li
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2022-04-13
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#include "align_ir_rgb_g2o.h"
+int main(int argc, char** argv) { return 0; }
---
+++ align_ir_rgb_g2o.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ *   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+ *
+ *   @Filename: align_ir_rgb_g2o.h
+ *
+ *   @Author: Shun Li
+ *
+ *   @Email: 2015097272@qq.com
+ *
+ *   @Date: 2022-04-13
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#ifndef ALIGN_IR_RGB_ALIGN_IR_RGB_G2O_H_
+#define ALIGN_IR_RGB_ALIGN_IR_RGB_G2O_H_
+
+#include <Eigen/Core>
+
+#endif  // ALIGN_IR_RGB_ALIGN_IR_RGB_G2O_H_
